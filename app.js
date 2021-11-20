32d950b2fee67689e62e5bb5b50c4b7a819a5bce
mfaza16 <mfaza16717@gmail.com>
2021-11-21 00:19:10 +0700

    Integrating modules to app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hi from the server');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('db is connected');
+    app.listen(port, console.log(`Server is running on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
